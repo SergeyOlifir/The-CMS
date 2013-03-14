4cf8819cda004e6a8bdaf90d06f6c28f312f3354
Juise_Man <Sergey.Olifir@gmail.com>
2013-03-14 23:51:40 +0200

    Add cache to ajax results

--- fuel/app/classes/controller/home/page.php
+++ fuel/app/classes/controller/home/page.php
@@ -7,25 +7,38 @@
 class Controller_Home_Page extends Controller_Homerest {
 	
 	function get_List($pageId = null) {
-		is_null($pageId) and $this->response(array('data' => "", 'popup' => ""), 404);
-		$page = Model_Page::find($pageId);
-		$result_content = View::forge("templates/{$this->template}/pages/list", array('page' => $page))->render();
-		$result_popup = "";//View::forge("templates/{$this->template}/pages/popup", array('page' => $page))->render();
-		$this->response(array('data' => $result_content, 'popup' => $result_popup), 200);
+		try {
+			$result_content = Cache::get('list' . $pageId);
+		} catch (\CacheNotFoundException $e) {
+			is_null($pageId) and $this->response(array('data' => "", 'popup' => ""), 404);
+			$page = Model_Page::find($pageId);
+			$result_content = View::forge("templates/{$this->template}/pages/list", array('page' => $page))->render();
+			Cache::set('list' . $pageId, $result_content, 3600 * 3);
+		}
+		$this->response(array('data' => $result_content), 200);
 	}
 	
 	public function get_tiles($pageId = null) {
-		is_null($pageId) and $this->response(array('data' => "", 'popup' => ""), 404);
-		$page = Model_Page::find($pageId);
-		$result_content = View::forge("templates/{$this->template}/pages/tile", array('page' => $page))->render();
-		$result_popup = "";//View::forge("templates/{$this->template}/pages/popup", array('page' => $page))->render();
-		$this->response(array('data' => $result_content, 'popup' => $result_popup), 200);
+		try {
+			$result_content = Cache::get('tile' . $pageId);
+		} catch (\CacheNotFoundException $e) {
+			is_null($pageId) and $this->response(array('data' => "", 'popup' => ""), 404);
+			$page = Model_Page::find($pageId);
+			$result_content = View::forge("templates/{$this->template}/pages/tile", array('page' => $page))->render();
+			Cache::set('tile' . $pageId, $result_content, 3600 * 3);
+		}
+		$this->response(array('data' => $result_content), 200);
 	}
 	
 	public function get_popup($content_id = null) {
-		is_null($content_id) and $this->response(array('data' => "", 'popup' => ""), 404);
-		$content = Model_Content::find($content_id);
-		$result_popup = View::forge("templates/{$this->template}/pages/popup", array('content' => $content))->render();
+		try {
+			$result_popup = Cache::get('popup' . $content_id);
+		} catch (\CacheNotFoundException $e) {
+			is_null($content_id) and $this->response(array('data' => "", 'popup' => ""), 404);
+			$content = Model_Content::find($content_id);
+			$result_popup = View::forge("templates/{$this->template}/pages/popup", array('content' => $content))->render();
+			Cache::set('popup' . $content_id, $result_popup, 3600 * 3);
+		}
 		$this->response(array('data' => $result_popup), 200);
 	}
 
