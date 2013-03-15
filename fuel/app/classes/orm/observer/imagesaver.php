ef9e166799cbff99b7c1722f76f9146f07295125
Juise_Man <Sergey.Olifir@gmail.com>
2013-03-15 17:40:14 +0200

    Refactioring image saver observer

--- fuel/app/classes/orm/observer/imagesaver.php
+++ fuel/app/classes/orm/observer/imagesaver.php
@@ -21,12 +21,9 @@ class Observer_ImageSaver extends Observer
 		$logo_path = $config['upload']['path'];
 		foreach( \Upload::get_files() as $file ) {
 				if($file['field'] == 'image') {
-					$size_name = "small";
 					$logo_name = $file['saved_as'];
-					$new_logo_name = str_replace(" ","_",md5(time()).'.'.$file['extension']);
-					\Image::load_and_resize("{$logo_path}/{$logo_name}", $config['sizes']['small'], "-{$size_name}", "{$logo_path}/{$new_logo_name}");
-					$extension  = \Str::lower($file['extension']);
-					$base_file_name = basename($logo_name, ".".$extension);
+					$new_logo_name = str_replace(".{$file['extension']}", "-cropt." . \Str::lower($file['extension']) , $file['saved_as']);
+					\Image::load("{$logo_path}/{$logo_name}")->resize($config['sizes']["small"]["width"], $config['sizes']["small"]["height"], true, false)->save("{$logo_path}/{$new_logo_name}");
 					$obj->image = $new_logo_name;
 					return true;
 				}
