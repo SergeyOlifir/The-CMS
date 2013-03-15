eb54b8bdb90bd752f5acfc6145b46467c40440ab
Juise_Man <Sergey.Olifir@gmail.com>
2013-03-15 02:22:32 +0200

    Remove _404_ route

--- fuel/app/config/routes.php
+++ fuel/app/config/routes.php
@@ -1,5 +1,5 @@
 <?php
 return array(
 	'_root_'  => 'home/index',  // The default route
-	'_404_'   => 'welcome/404',    // The main 404 route
+	//'_404_'   => 'welcome/404',    // The main 404 route
 );
