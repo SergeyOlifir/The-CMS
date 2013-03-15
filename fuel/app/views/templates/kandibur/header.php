25568985b1f31f065f104251f16efa7fd7d47d6f
Juise_Man <Sergey.Olifir@gmail.com>
2013-03-15 14:04:22 +0200

    Add clock to kandibur template

--- fuel/app/views/templates/kandibur/header.php
+++ fuel/app/views/templates/kandibur/header.php
@@ -1,6 +1,9 @@
 <div class="header-wrapper clearfix">
 	<h1 class="left">Кандибур</h1>
-	<div>
+	<div class='left'>
 
+	</div>
+	<div class="movie-wrapper right">
+		<embed width="100px" height="100px" name="plugin" src="<?= Uri::create('assets/flash/templates/kandibur/clock08.swf'); ?>" type="application/x-shockwave-flash">
 	</div>
 </div>
