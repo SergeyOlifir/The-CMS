fe535ec8a98f2bec9a72f410575aa8095553ac2d
Anna <ann.pylypas@yandex.ru>
2013-03-26 11:52:46 +0200

    Add carousel in header template 'Kandibur'

--- fuel/app/views/templates/kandibur/header.php
+++ fuel/app/views/templates/kandibur/header.php
@@ -1,8 +1,6 @@
 <div class="header-wrapper clearfix">
 	<h1 class="left">Кандибур</h1>
-	<div class='left'>
-
-	</div>
+		<?= render('templates/kandibur/columns/carusel',array()); ?>
 	<div class="movie-wrapper right">
 		<embed width="105px" height="105px" name="plugin" src="<?= Uri::create('assets/flash/templates/kandibur/clock08.swf'); ?>" type="application/x-shockwave-flash">
 	</div>
