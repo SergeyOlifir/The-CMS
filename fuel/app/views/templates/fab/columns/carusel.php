160e8c10f97c731a6db83c5047b3c78353870b50
Anna <ann.pylypas@yandex.ru>
2013-04-02 16:29:54 +0300

    Add carousel for 'fab'

---
+++ fuel/app/views/templates/fab/columns/carusel.php
@@ -0,0 +1,33 @@
+<div class="carusel right">
+    <ul>
+        <li>
+            <a data-title="ТМ АГРО - СОЮЗ - 10pt, bold; ссылка" data-info="Краткое описание работ для клиента, информация о услугах которые делали для этого клиента - 10pt, normal;" href="#raws">
+                <?= Html::img("assets/img/templates/fab/galery_images/img1.png"); ?>
+            </a>
+        </li>
+        <li>
+            <a data-title="Paradise - 10pt, bold; ссылка" data-info="Краткое описание работ для клиента, информация о услугах которые делали для этого клиента - 10pt, normal;" href="#raws">
+                <?= Html::img("assets/img/templates/fab/galery_images/img2.png"); ?>
+            </a>
+        </li>
+        <li>
+            <a data-title="Almaz - 10pt, bold; ссылка" data-info="Краткое описание работ для клиента, информация о услугах которые делали для этого клиента - 10pt, normal;" href="#raws">
+                <?= Html::img("assets/img/templates/fab/galery_images/img3.png"); ?>
+            </a>
+        </li>
+        <li>
+            <a data-title="Appolo - 10pt, bold; ссылка" data-info="Краткое описание работ для клиента, информация о услугах которые делали для этого клиента - 10pt, normal;" href="#raws">
+                <?= Html::img("assets/img/templates/fab/galery_images/img4.png"); ?>
+            </a>
+        </li>
+        <li>
+            <a data-title="Акватория - 10pt, bold; ссылка" data-info="Краткое описание работ для клиента, информация о услугах которые делали для этого клиента - 10pt, normal;" href="#raws">
+                <?= Html::img("assets/img/templates/fab/galery_images/img5.png"); ?>
+            </a>
+        </li>
+    </ul>
+</div>
+<div class="hint">
+    <p class="hint-title"><a href="#"></a></p>
+    <p class="hint-info"></p>
+</div>
