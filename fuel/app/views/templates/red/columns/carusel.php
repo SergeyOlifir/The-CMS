befe759a3e3e9d14de86702f95968bd4d0763326
Anna <ann.pylypas@yandex.ru>
2013-04-02 15:25:58 +0300

    Modified carousel for 'red'

--- fuel/app/views/templates/red/columns/carusel.php
+++ fuel/app/views/templates/red/columns/carusel.php
@@ -1,14 +1,58 @@
 <div class="carusel">
     <ul>
-        <li><?= Html::img("assets/img/templates/red/galery_images/Im_gl_01.jpg"); ?></li>
-        <li><?= Html::img("assets/img/templates/red/galery_images/Im_gl_02.jpg"); ?></li>
-        <li><?= Html::img("assets/img/templates/red/galery_images/Im_gl_03.jpg"); ?></li>
-        <li><?= Html::img("assets/img/templates/red/galery_images/Im_gl_04.jpg"); ?></li>
-        <li><?= Html::img("assets/img/templates/red/galery_images/Im_gl_05.jpg"); ?></li>
-        <li><?= Html::img("assets/img/templates/red/galery_images/Im_gl_06.jpg"); ?></li>
-        <li><?= Html::img("assets/img/templates/red/galery_images/Im_gl_07.jpg"); ?></li>
-        <li><?= Html::img("assets/img/templates/red/galery_images/Im_gl_08.jpg"); ?></li>
-        <li><?= Html::img("assets/img/templates/red/galery_images/Im_gl_09.jpg"); ?></li>
-        <li><?= Html::img("assets/img/templates/red/galery_images/Im_gl_10.jpg"); ?></li>
+        <li>
+            <a data-title="Блины с мясом - 10pt, bold; ссылка" data-info="Краткое описание состава продукции, ингредиенты которые входят в продукт - 10pt, normal;" href="#">
+                <?= Html::img("assets/img/templates/red/galery_images/Im_gl_01.jpg"); ?>
+            </a>
+        </li>
+        <li>
+            <a data-title="Блины с мясом - 10pt, bold; ссылка" data-info="Краткое описание состава продукции, ингредиенты которые входят в продукт - 10pt, normal;" href="#">
+                <?= Html::img("assets/img/templates/red/galery_images/Im_gl_02.jpg"); ?>
+            </a>
+        </li>
+        <li>
+            <a data-title="Блины с мясом - 10pt, bold; ссылка" data-info="Краткое описание состава продукции, ингредиенты которые входят в продукт - 10pt, normal;" href="#">
+                <?= Html::img("assets/img/templates/red/galery_images/Im_gl_03.jpg"); ?>
+            </a>
+        </li>
+        <li>
+            <a data-title="Блины с мясом - 10pt, bold; ссылка" data-info="Краткое описание состава продукции, ингредиенты которые входят в продукт - 10pt, normal;" href="#">
+                <?= Html::img("assets/img/templates/red/galery_images/Im_gl_04.jpg"); ?>
+            </a>
+        </li>
+        <li>
+            <a data-title="Блины с мясом - 10pt, bold; ссылка" data-info="Краткое описание состава продукции, ингредиенты которые входят в продукт - 10pt, normal;" href="#">
+                <?= Html::img("assets/img/templates/red/galery_images/Im_gl_05.jpg"); ?>
+            </a>
+        </li>
+        <li>
+            <a data-title="Блины с мясом - 10pt, bold; ссылка" data-info="Краткое описание состава продукции, ингредиенты которые входят в продукт - 10pt, normal;" href="#">
+                <?= Html::img("assets/img/templates/red/galery_images/Im_gl_06.jpg"); ?>
+            </a>
+        </li>
+        <li>
+            <a data-title="Блины с мясом - 10pt, bold; ссылка" data-info="Краткое описание состава продукции, ингредиенты которые входят в продукт - 10pt, normal;" href="#">
+                <?= Html::img("assets/img/templates/red/galery_images/Im_gl_07.jpg"); ?>
+            </a>
+        </li>
+        <li>
+            <a data-title="Блины с мясом - 10pt, bold; ссылка" data-info="Краткое описание состава продукции, ингредиенты которые входят в продукт - 10pt, normal;" href="#">
+                <?= Html::img("assets/img/templates/red/galery_images/Im_gl_08.jpg"); ?>
+            </a>
+        </li>
+        <li>
+            <a data-title="Блины с мясом - 10pt, bold; ссылка" data-info="Краткое описание состава продукции, ингредиенты которые входят в продукт - 10pt, normal;" href="#">
+                <?= Html::img("assets/img/templates/red/galery_images/Im_gl_09.jpg"); ?>
+            </a>
+        </li>
+        <li>
+            <a data-title="Блины с мясом - 10pt, bold; ссылка" data-info="Краткое описание состава продукции, ингредиенты которые входят в продукт - 10pt, normal;" href="#">
+                <?= Html::img("assets/img/templates/red/galery_images/Im_gl_10.jpg"); ?>
+            </a>
+        </li>
     </ul>
+</div>
+<div class="hint">
+    <p class="hint-title"><a href="#"></a></p>
+    <p class="hint-info"></p>
 </div>
--- public/assets/js/templates/red/content-loader.js
+++ public/assets/js/templates/red/content-loader.js
@@ -4,9 +4,29 @@ $(document).ready( function() {
 	    $(".carusel").jCarouselLite({
 	       'auto': 1300,
 	       'visible': 7,
+	       'hoverPause': true
 	    });
 	});
 		
+	$('.carusel ul').on('mouseenter','li', function(e) {
+		var coord_x = e.clientX + (document.documentElement.scrollLeft || document.body.scrollLeft) - document.documentElement.clientLeft - 22;
+		$('.hint p.hint-title a').html($(this).find('a').attr('data-title'));
+		$('.hint p.hint-info').html($(this).find('a').attr('data-info'));
+		$('.hint').css('left',coord_x).fadeIn(300);
+	}).on('mouseleave','li',function(e) {
+		var curr_x = e.clientX + (document.documentElement.scrollLeft || document.body.scrollLeft);
+		var curr_y = e.clientY + (document.documentElement.scrollTop || document.body.scrollTop);
+		$('.hint').offset(function(i,val){
+			if ((curr_x < val.left) || (curr_x > val.left + this.width) || (curr_y < val.top - 10) || (curr_y > val.top + this.height)) {
+				$('.hint').fadeOut(200);
+			}
+		});
+	});
+
+	$('.hint').on('mouseleave',function() {
+		$('.hint').fadeOut(200);
+	})
+
 	$( ".main-menu" ).click(function() {
         $( ".main-menu-content" ).toggleClass( "active", 500 );
     });
