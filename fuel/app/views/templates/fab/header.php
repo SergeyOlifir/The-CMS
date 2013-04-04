bd404dd5ee0f48e5ada56e7bcdad6619acf4d5a3
Anna <ann.pylypas@yandex.ru>
2013-04-04 17:34:23 +0300

    Add title over carousel for 'fab'

--- fuel/app/views/templates/fab/header.php
+++ fuel/app/views/templates/fab/header.php
@@ -1,5 +1,8 @@
 <div class="header-wrapper clearfix">
 	<h1 class="left">Faberge-media</h1>
-	<span class="slogan right">Рекламных дел мастера!</span>
+	<div class="slogan right">
+		<span class="left">Faberge!</span>
+		<span class="right">Рекламных дел мастера!</span>
+	</div>
 	<?= render('templates/fab/columns/carusel',array()); ?>
 </div>
