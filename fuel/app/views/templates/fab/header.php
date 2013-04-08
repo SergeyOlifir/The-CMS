398b224dd25ac9fbd8a5491b9a3b63dd331b0ce5
Anna <ann.pylypas@yandex.ru>
2013-04-08 12:41:14 +0300

    Add title 'client-list' for 'fab'

--- fuel/app/views/templates/fab/header.php
+++ fuel/app/views/templates/fab/header.php
@@ -1,7 +1,7 @@
 <div class="header-wrapper clearfix">
 	<h1 class="left">Faberge-media</h1>
 	<div class="slogan right">
-		<span class="left">Faberge!</span>
+		<span class="left">Клиент-лист</span>
 		<span class="right">Рекламных дел мастера!</span>
 	</div>
 	<?= render('templates/fab/columns/carusel',array()); ?>
