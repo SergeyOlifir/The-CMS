60c556eb8fc76b401b0e73107a362d11b275919a
Julia <julia@julia-K53SM.(none)>
2013-04-19 17:37:08 +0300

    Categories on home page paddings and trncate added for text

--- fuel/app/views/templates/the42template/home/partials/categories.php
+++ fuel/app/views/templates/the42template/home/partials/categories.php
@@ -1,14 +1,12 @@
 <? if(isset($content)): ?>
-    <div class='app-categories clearfix'>
+    <div class='app-categories clearfix wrapper-padding'>
         <ul>
             <? foreach($content as $category) { ?>
                 <li class="category img-circle">
-                    <a class="image img-circle"></a>
-                    <a class="name">
-                        <?= $category->name; ?>
-                    </a>
+                    <?= \Fuel\Core\Html::anchor("/", "", array('class' =>'image img-circle')); ?>
+                    <h3><?= \Fuel\Core\Html::anchor("/", $category->name, array('class' =>'name')); ?></h3>
                     <span class='description'>
-                        <?= $category->header; ?>
+                        <?= Str::truncate($category->header, 100, '...'); ?>
                     </span>
                 </li>
             <? } ?>
