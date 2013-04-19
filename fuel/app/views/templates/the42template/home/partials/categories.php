f69b57728f2988f587204d930d337f4be8959962
Julia <julia@julia-K53SM.(none)>
2013-04-19 20:01:08 +0300

    styles for gallery fixing in progress

--- fuel/app/views/templates/the42template/home/partials/categories.php
+++ fuel/app/views/templates/the42template/home/partials/categories.php
@@ -1,8 +1,8 @@
 <? if(isset($content)): ?>
-    <div class='app-categories clearfix wrapper-padding'>
-        <ul>
+    <div class='app-categories wrapper-padding'>
+        <ul class="clearfix">
             <? foreach($content as $category) { ?>
-                <li class="category img-circle">
+                <li class="category left img-circle">
                     <?= \Fuel\Core\Html::anchor("/", "", array('class' =>'image img-circle')); ?>
                     <h3><?= \Fuel\Core\Html::anchor("/", $category->name, array('class' =>'name')); ?></h3>
                     <span class='description'>
