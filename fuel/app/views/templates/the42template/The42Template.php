02032b790a0c777fce74f1b062b950e8a586570c
Julia <julia@julia-K53SM.(none)>
2013-04-24 18:13:57 +0300

    js not in need removed

--- fuel/app/views/templates/the42template/The42Template.php
+++ fuel/app/views/templates/the42template/The42Template.php
@@ -19,8 +19,7 @@
 		<?= Asset::js('jquery.reveal.js'); ?>
 		<?= Asset::js('jquery.fancybox.pack.js'); ?>
 		<?= Asset::js('jquery.fancybox-buttons.js'); ?>
-        <?= Asset::js('templates/the42template/jquery.jscrollpane.min.js'); ?>
-        <?= Asset::js('templates/the42template/content-loader.js'); ?>
+        <?=TCTheme::add_js('content-loader.js'); ?>
         <?= Asset::js('bootstrap.min.js'); ?>
 		<?= Asset::render('javascripts'); ?>
 	</head>
