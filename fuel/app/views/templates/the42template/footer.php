119760e5392254f38ab3d3a4e113c8e1de33198b
Julia <julia@julia-K53SM.(none)>
2013-04-25 16:08:00 +0300

    footer text and logo added

--- fuel/app/views/templates/the42template/footer.php
+++ fuel/app/views/templates/the42template/footer.php
@@ -1,45 +1,55 @@
 <div class="all-wrapper">
-    <div class="wrapper-padding clearfix">
-        <div class="container">
-            <div class="one_half">
-                <div id="infiniti_1" class="infinitebox toright">
-                    <div class="ib-draws">
-                        <div class="ib-draw" data-targetwrap="#ib-draw-2">
-                            <div class="ib-icon twitter-icon"></div>
-                        </div>
-                        <div class="ib-draw" data-targetwrap="#ib-draw-1">
-                            <div class="ib-icon facebook-icon"></div>
-                        </div>
-                        <div class="ib-draw" data-targetwrap="#ib-draw-3">
-                            <div class="ib-icon comment-icon"></div>
-                        </div>
-                        <div class="ib-draw" data-targetwrap="#ib-draw-4">
-                            <div class="ib-icon article-icon"></div>
-                        </div>
+        <div class="one_half">
+            <div id="infiniti_1" class="infinitebox toright">
+                <div class="ib-draws">
+                    <div class="ib-draw" data-targetwrap="#ib-draw-2">
+                        <div class="ib-icon twitter-icon"></div>
                     </div>
+                    <div class="ib-draw" data-targetwrap="#ib-draw-1">
+                        <div class="ib-icon facebook-icon"></div>
+                    </div>
+                    <div class="ib-draw" data-targetwrap="#ib-draw-3">
+                        <div class="ib-icon comment-icon"></div>
+                    </div>
+                    <div class="ib-draw" data-targetwrap="#ib-draw-4">
+                        <div class="ib-icon article-icon"></div>
+                    </div>
+                </div>
 
-                    <div class="ib-content">
-                        <div id="ib-draw-1" class="ib-content-wrap ">
-                                <h3>Hello world</h3>
-                        </div>
-                        <div id="ib-draw-2" class="ib-content-wrap">
-                                <h3>Hello world</h3>
-                        </div>
-                        <div id="ib-draw-3" class="ib-content-wrap">
-                                <h3>Hello world</h3>
-                        </div>
-                        <div id="ib-draw-4" class="ib-content-wrap">
-                                <h3>Hello world</h3>
-                        </div>
-                        <div class="ib-close"> </div>
+                <div class="ib-content">
+                    <div id="ib-draw-1" class="ib-content-wrap ">
+                            <h3>Hello world</h3>
                     </div>
+                    <div id="ib-draw-2" class="ib-content-wrap">
+                            <h3>Hello world</h3>
+                    </div>
+                    <div id="ib-draw-3" class="ib-content-wrap">
+                            <h3>Hello world</h3>
+                    </div>
+                    <div id="ib-draw-4" class="ib-content-wrap">
+                            <h3>Hello world</h3>
+                    </div>
+                    <div class="ib-close"> </div>
                 </div>
             </div>
         </div>
 
         <div class="one_half">
-
+            <div class="address">
+                <p>
+                    Welcome In 42 Development Studio,
+                    <br/>
+                    skype: 42devstudio
+                    <br/>
+                    Phone: 1-800-245.356 / Fax: 1-800-245.357
+                    <br/>
+                    Email:
+                    <a href="mailto:42devstudio@gmail.com">42devstudio@gmail.com</a>
+                </p>
+            </div>
+            <div class="logo">
+                <?= \Fuel\Core\Html::anchor("/", "", array('class' =>'logo-footer')); ?>
+            </div>
         </div>
 
-    </div>
 </div>
