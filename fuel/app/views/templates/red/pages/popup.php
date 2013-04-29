a597982e947c41ace09add2a5b9518722832603a
Anna <ann.pylypas@yandex.ru>
2013-04-29 15:12:13 +0300

    Modified popup for 'kandibur','fab','red'

--- fuel/app/views/templates/red/pages/popup.php
+++ fuel/app/views/templates/red/pages/popup.php
@@ -1,16 +1,17 @@
-<h3><?= $localcontent->name; ?></h3>
-<div class="popup-content scroll-news-index"><?= $localcontent->description; ?></div>
+<? $curr_lang_id = TCLocale::get_current_leng_id(); ?>
+<h3><?= $content->name; ?></h3>
+<div class="popup-content scroll-news-index"><?= $content->description; ?></div>
 
 <? $contents = $content->related_content; ?>
 <ul class="tile clearfix">
 	<? foreach ($contents as $content): ?>
 		<li>
-			<a id ="<?= $content->id; ?>" data-reveal-id="contents_popup" href="#" content_id="<?= $content->id; ?>" class="show-popup">
+			<a id="<?= $content->id; ?>" data-reveal-id="contents_popup" href="#" content_id="<?= $content->id; ?>" class="show-popup">
 				<div class="img-wrapper">
 					<?= Html::img("files/{$content->image}"); ?>
 				</div>
 				<h3 class="header">
-					<?= $content->get_translation(1)->name; ?>
+					<?= $content->name; ?>
 				</h3>
 			</a>
 		</li>
