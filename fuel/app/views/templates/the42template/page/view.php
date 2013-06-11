51309ce81d1651a6676bca3f1d029d2b50efddfe
Anna <ann.pylypas@yandex.ru>
2013-06-11 16:33:11 +0300

    Added the display of the description page for 'the42template'

--- fuel/app/views/templates/the42template/page/view.php
+++ fuel/app/views/templates/the42template/page/view.php
@@ -1,9 +1,12 @@
-<section class="submenu">
-	<h2>Portfolio</h2>
-	<?= \Fuel\Core\Html::anchor("home/content/change/tile/{$page->alias}", "Tile", array('class' => 'tile')); ?>
-	<?= \Fuel\Core\Html::anchor("home/content/change/list/{$page->alias}", "List", array('class' => 'list')); ?>
-</section>
+<? if($page->description !== null): ?>
+    <?= $page->description; ?>
+<? endif; ?>
 <? if($content): ?>
+	<section class="submenu">
+		<h2>Portfolio</h2>
+		<?= \Fuel\Core\Html::anchor("home/content/change/tile/{$page->alias}", "Tile", array('class' => 'tile')); ?>
+		<?= \Fuel\Core\Html::anchor("home/content/change/list/{$page->alias}", "List", array('class' => 'list')); ?>
+	</section>
 	<? if(count($content) > 0): ?>
 		<? if (Session::get('tile')): ?>
 		    <?= TCTheme::render("content/tiles", array('content' => $content, 'page' => $page)); ?>
