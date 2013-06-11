d2a547dd838e06556af1a3e41cdee76babc8f5d9
Anna <ann.pylypas@yandex.ru>
2013-06-11 16:39:49 +0300

    Modified the view content for 'the 42template'

--- fuel/app/views/templates/the42template/page/view.php
+++ fuel/app/views/templates/the42template/page/view.php
@@ -2,12 +2,12 @@
     <?= $page->description; ?>
 <? endif; ?>
 <? if($content): ?>
-	<section class="submenu">
-		<h2>Portfolio</h2>
-		<?= \Fuel\Core\Html::anchor("home/content/change/tile/{$page->alias}", "Tile", array('class' => 'tile')); ?>
-		<?= \Fuel\Core\Html::anchor("home/content/change/list/{$page->alias}", "List", array('class' => 'list')); ?>
-	</section>
-	<? if(count($content) > 0): ?>
+	<? if(count($page->content) > 0): ?>
+		<section class="submenu">
+			<h2>Portfolio</h2>
+			<?= \Fuel\Core\Html::anchor("home/content/change/tile/{$page->alias}", "Tile", array('class' => 'tile')); ?>
+			<?= \Fuel\Core\Html::anchor("home/content/change/list/{$page->alias}", "List", array('class' => 'list')); ?>
+		</section>
 		<? if (Session::get('tile')): ?>
 		    <?= TCTheme::render("content/tiles", array('content' => $content, 'page' => $page)); ?>
 		<? else: ?>
