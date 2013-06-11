98efcc60bfda742ac8c439f8d0374d6609c7ed78
Anna <ann.pylypas@yandex.ru>
2013-06-11 16:23:53 +0300

    Modified structure for portfolio of 'the42template'

--- fuel/app/views/templates/the42template/page/view.php
+++ fuel/app/views/templates/the42template/page/view.php
@@ -1,24 +1,15 @@
-<? if($page->description !== null): ?>
-    <?= $page->description; ?>
-<? endif; ?>
-<? if($page->content): ?>
-    <? if(count($page->content) > 0): ?>
-	<section class="projects wrapper-padding clearfix">
-	    <header>
-		<h1><legend>Похожие проекты</legend></h1>
-	    </header>
-	    <? foreach ($page->content as $project): ?>
-		<article class="project tile left">
-		    <div class="logo-wrapper">
-			<?= Html::img("files/{$project['image']}"); ?>
-		    </div>
-		    <header class="name">
-			<h1><?= \Fuel\Core\Html::anchor("/home/content/view/{$project->id}", $project['name'], array('class' => 'project-link')); ?></h1>
-			<p><?= \Fuel\Core\Html::anchor("home/category/view/{$project['page']->alias}", $project['page']->name, array('class' => 'project-link')); ?></p>
-		    </header>
-		    <?= \Fuel\Core\Html::anchor("#", "More", array('class' => 'more-link')); ?>
-		</article>
-	    <? endforeach; ?>
-	</section>
-    <? endif; ?>
-<? endif; ?>
+<section class="submenu">
+	<h2>Portfolio</h2>
+	<?= \Fuel\Core\Html::anchor("home/content/change/tile/{$page->alias}", "Tile", array('class' => 'tile')); ?>
+	<?= \Fuel\Core\Html::anchor("home/content/change/list/{$page->alias}", "List", array('class' => 'list')); ?>
+</section>
+<? if($content): ?>
+	<? if(count($content) > 0): ?>
+		<? if (Session::get('tile')): ?>
+		    <?= TCTheme::render("content/tiles", array('content' => $content, 'page' => $page)); ?>
+		<? else: ?>
+		    <?= TCTheme::render("content/list", array('content' => $content, 'page' => $page)); ?>
+		<? endif;?>
+		<?= $pagination; ?>
+	<? endif; ?>
+<? endif; ?> 
