c02a866741eed0814ff0864fc7266fc7eca55af5
Juise_Man <Sergey.Olifir@gmail.com>
2013-10-17 15:45:32 +0300

    karuselka4

--- fuel/app/classes/controller/home.php
+++ fuel/app/classes/controller/home.php
@@ -1,22 +1,24 @@
 <?php
+
 /**
  * Description of home
  *
  * @author juise_man
  */
 class Controller_Home extends Controller_Application {
-	
-	function before() {
-		$this->template = TCTheme::main_view();
-		parent::before();
-	}
-			
-	function action_index() {
-            self::$current_page = "Home";
-            $content = \Model_Category::find('all');
-	    	$this->template->content = TCTheme::load_view('home/partials/gallery');
-            $this->template->content .= \Fuel\Core\View::forge("templates/{$this::$template_name}/home/partials/categories",array('content' => $content));
-	}
+
+    function before() {
+        $this->template = TCTheme::main_view();
+        parent::before();
+    }
+
+    function action_index() {
+        self::$current_page = "Home";
+        $content = \Model_Category::find('all');
+        $this->template->content = TCTheme::load_view('home/partials/gallery');
+        //$this->template->content .= \Fuel\Core\View::forge("templates/{$this::$template_name}/home/partials/categories", array('content' => $content));
+    }
+
 }
 
 ?>
