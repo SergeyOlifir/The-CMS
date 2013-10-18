b180afba91fe14b24cad3e39384fe96e86a0a741
Juise_Man <Sergey.Olifir@gmail.com>
2013-10-18 15:59:47 +0300

    url banner zhitn

--- fuel/app/views/templates/default/header.php
+++ fuel/app/views/templates/default/header.php
@@ -1,7 +1,7 @@
 <div class="header-wrapper clearfix">
 					<h1 class="left">Житница</h1>
 					<div>
-						<a href="/home/content/view/8" target="_blank">
+						<a href="http://karuselfest.com.ua/home/content/view/8" target="_blank">
 							<img alt="" src="assets/img/templates/default/ban_karuselfest.jpg" style="border: 1px solid #996633;
 																					border-radius: 12px 12px 12px 12px;
 																					float: left;
