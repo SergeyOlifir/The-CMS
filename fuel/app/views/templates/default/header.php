38e32d523215178ed7eb426e1b4e9da6851ef05e
Juise_Man <Sergey.Olifir@gmail.com>
2013-10-18 15:57:23 +0300

    banner zhitninca

--- fuel/app/views/templates/default/header.php
+++ fuel/app/views/templates/default/header.php
@@ -1,8 +1,8 @@
 <div class="header-wrapper clearfix">
 					<h1 class="left">Житница</h1>
 					<div>
-						<a href="http://umimarket.com.ua/index.php?route=product/category&amp;path=119" target="_blank">
-							<img alt="" src="files/images/ban_tolokno.jpg" style="border: 1px solid #996633;
+						<a href="/home/content/view/8" target="_blank">
+							<img alt="" src="assets/img/templates/default/ban_karuselfest.jpg" style="border: 1px solid #996633;
 																					border-radius: 12px 12px 12px 12px;
 																					float: left;
 																					height: 90px;
