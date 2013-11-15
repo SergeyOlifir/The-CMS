e3f69ad0ce4286d8678e47f63acb2905545996e0
Juise_Man <Sergey.Olifir@gmail.com>
2013-11-15 19:33:44 +0200

    logo a

--- fuel/app/views/templates/default/header.php
+++ fuel/app/views/templates/default/header.php
@@ -1,5 +1,5 @@
 <div class="header-wrapper clearfix">
-					<h1 class="left">Житница</h1>
+					<a href="/"><h1 class="left">Житница</h1></a>
 					<div>
 						<a href="http://karuselfest.com.ua/home/content/view/8" target="_blank">
 							<img alt="" src="assets/img/templates/default/ban_karuselfest.jpg" style="border: 1px solid #996633;
